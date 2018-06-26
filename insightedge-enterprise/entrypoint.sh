c0e07525f77b45cf1ccf77c2be84356f3d1f4440
talmelamud <tal.melamud@gigaspaces.com>
2018-06-26 14:18:26 +0300

    GS-13582 Support for docker public/private IP

--- insightedge-enterprise/entrypoint.sh
+++ insightedge-enterprise/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+SPARK_JAVA_OPTIONS_FILE="${GS_HOME}/insightedge/spark/conf/java-opts"
+
 if [ -z "$XAP_MANAGER_SERVERS" ]; then
 	export XAP_MANAGER_SERVERS=$HOSTNAME
 fi
+
+if [ -f "${SPARK_JAVA_OPTIONS_FILE}" ]; then
+	rm ${SPARK_JAVA_OPTIONS_FILE}
+fi
+
+echo "${EXT_JAVA_OPTIONS}" >> ${SPARK_JAVA_OPTIONS_FILE}
 
 /opt/gigaspaces/bin/insightedge $*
