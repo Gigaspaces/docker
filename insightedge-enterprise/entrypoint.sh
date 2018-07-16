42b474ff966f29227c150e3c430d3af8b538e28e
alonshoham <alon.shoham@gmail.com>
2018-07-16 15:56:01 +0300

    GS-13582 Support for docker public/private IP - fixed entry point

--- insightedge-enterprise/entrypoint.sh
+++ insightedge-enterprise/entrypoint.sh
@@ -3,7 +3,11 @@
 SPARK_JAVA_OPTIONS_FILE="${GS_HOME}/insightedge/spark/conf/java-opts"
 
 if [ -z "$XAP_MANAGER_SERVERS" ]; then
-	export XAP_MANAGER_SERVERS=$HOSTNAME
+    if [ -z "$XAP_PUBLIC_HOST"  ]; then
+	    export XAP_MANAGER_SERVERS=$HOSTNAME
+    else
+        export XAP_MANAGER_SERVERS=$XAP_PUBLIC_HOST
+    fi
 fi
 
 if [ -f "${SPARK_JAVA_OPTIONS_FILE}" ]; then
--- xap-enterprise/entrypoint.sh
+++ xap-enterprise/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ -z "$XAP_MANAGER_SERVERS" ]; then
-	export XAP_MANAGER_SERVERS=$HOSTNAME
+    if [ -z "$XAP_PUBLIC_HOST"  ]; then
+	    export XAP_MANAGER_SERVERS=$HOSTNAME
+    else
+        export XAP_MANAGER_SERVERS=$XAP_PUBLIC_HOST
+    fi
 fi
 
 /opt/gigaspaces/bin/xap $*
