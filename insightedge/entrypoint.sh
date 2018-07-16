0f20eeda30374274627706e4f459934b0e5b4314
alonshoham <alon.shoham@gmail.com>
2018-07-16 09:18:36 +0300

    Inject xap extra java options to spark
    (cherry picked from commit 5be5293)

--- insightedge/entrypoint.sh
+++ insightedge/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+SPARK_JAVA_OPTIONS_FILE="${GS_HOME}/insightedge/spark/conf/java-opts"
+
+if [ -f "${SPARK_JAVA_OPTIONS_FILE}" ]; then
+	rm ${SPARK_JAVA_OPTIONS_FILE}
+fi
+
+echo "${EXT_JAVA_OPTIONS}" >> ${SPARK_JAVA_OPTIONS_FILE}
+
 /opt/gigaspaces/bin/insightedge $*
