6e14e813055f4a32a6f8fe1c68055f11324c617b
moran <moran@gigaspaces.com>
2018-10-15 15:35:36 +0300

    GS-13661 SPARK_MOUNTED_CLASSPATH contains incorrect semicolon on Windows. Apply fix described in Apache Spark JIRA.

--- insightedge-enterprise/docker-entrypoint.sh
+++ insightedge-enterprise/docker-entrypoint.sh
@@ -20,6 +20,13 @@ case "$CMD_ARGS" in
     )
 esac
 
+# WORKAROUND FOR WINDOWS BUG USING SPARK SUBMIT
+# https://issues.apache.org/jira/browse/SPARK-24599
+if [ -n "$SPARK_MOUNTED_CLASSPATH" ]; then
+  SPARK_MOUNTED_CLASSPATH_FIX=$(echo "$SPARK_MOUNTED_CLASSPATH" | tr ';' ':')
+  export SPARK_MOUNTED_CLASSPATH="${SPARK_MOUNTED_CLASSPATH_FIX}"
+fi
+
 # Execute the container CMD under tini for better hygiene
 echo "Executing command: ${CMD[@]}" 1>&2
 exec /sbin/tini -s -- "${CMD[@]}"
